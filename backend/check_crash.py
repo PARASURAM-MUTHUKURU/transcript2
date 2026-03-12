11c132f54c4e94eedbeb9f84e6c8af71dc7a247a
PARASURAM-MUTHUKURU <m.parasuram5206etf@gmail.com>
2026-03-12 20:36:42 +0530

    deled unwanted files

--- backend/check_crash.py
+++
@@ -1,8 +0,0 @@
-try:
-    print("Attempting to import app from main...")
-    from main import app
-    print("Import successful!")
-except Exception as e:
-    import traceback
-    print("IMPORT FAILED!")
-    traceback.print_exc()
